b2255c0dcb049d5fc3ee5c7bb8126fc6ed40924c
Sairaj Goutham Dhullipalla <dhullipa@msu.edu>
2025-01-06 18:01:27 -0500

    Delete new directory

--- new/DocumentManagement/DocumentManagement.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS documents;
-
-CREATE TABLE documents(
-    username TEXT,
-    file_name TEXT,
-    body TEXT, 
-    groups TEXT
-);
--- new/Logging/Logging.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS logs;
-
-CREATE TABLE logs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    event_type TEXT NOT NULL,
-    file_name TEXT,
-    event_date REAL NOT NULL
-);
